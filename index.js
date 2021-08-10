1a09e8234c84db495a4129743ca11c53a5ed2a19
Jeremiah <jsnidar@gmail.com>
2021-08-10 09:06:03 -0500

    commit tree 74ce93f852f62afcca99717bb116f073a2a1e6c8

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    let userObj = {
+        name: `${userName}`,
+        email: `${userEmail}`
+    }
+
+    // appendUserInfo = function () {
+    //     const userId = document.createElement('div')
+    //     userId.innerText = object.id
+    //     document.body.appendChild(userId);
+    // }
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept": "application/json"
+        },
+        body:JSON.stringify(userObj) 
+    })
+    .then(response => response.json())
+    .then((object) => {
+        let userId = document.createElement('div')
+        userId.innerHTML = object.id
+        document.body.appendChild(userId);
+    })
+    .catch(function(error) {
+        let errorMsg = document.createElement('div')
+        errorMsg.innerHTML = ("Unauthorized Access")
+        document.body.appendChild(errorMsg)
+        console.log(error);
+    });
+}
+
